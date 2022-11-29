3adf3b727459656b1838234b8008b7709d914e31
Giuliano <giulianovieiramartins@gmail.com>
2022-11-29 15:52:58 -0300

    feat :sparkles:

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import './App.css';
+import { Route, Switch } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import ProviderRecipes from './context/ProviderRecipes';
+import Login from './components/Login';
 
 function App() {
   return (
     <ProviderRecipes>
-      <div>queijo</div>
+      <Switch>
+        <Route exact path="/" component={ Login } />
+        {/* <Route exact path="/game" component={ Game } /> */}
+      </Switch>
     </ProviderRecipes>
   );
 }
