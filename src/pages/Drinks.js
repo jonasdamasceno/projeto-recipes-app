105b2f0e07b2fbb415f01124386aab625dfd5c4f
Ivan <ivan.dadalto@gmail.com>
2022-12-01 17:11:55 -0300

    feat: show on screen first 12 recipes from drinks or meals

--- src/pages/Drinks.js
+++ src/pages/Drinks.js
@@ -2,29 +2,32 @@ import React, { useContext, useEffect } from 'react';
 import ContextRecipes from '../context/ContextRecipes';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
+import { requestDrinks } from '../service/RequestAPI';
+import Recipes from './Recipes';
 
 export default function Drinks() {
-  const { setTitle, requestDrink } = useContext(ContextRecipes);
+  const { setTitle, setRecipesData, requestDrink } = useContext(ContextRecipes);
   const TWELVE = 12;
   useEffect(() => {
     setTitle('Drinks');
-  }, [setTitle]);
+    requestDrinks().then((drink) => setRecipesData(drink.drinks));
+  }, [setTitle, setRecipesData]);
 
   return (
     <div>
       <Header />
       <div>
         {(requestDrink.length > 1)
-        && requestDrink.slice(0, TWELVE).map((drink, index) => (
-          <div key={ drink.idDrink } data-testid={ `${index}-recipe-card` }>
-            <img
-              src={ drink.strDrinkThumb }
-              alt="imagem do drink"
-              data-testid={ `${index}-card-img` }
-            />
-            <p data-testid={ `${index}-card-name` }>{drink.strDrink}</p>
-          </div>
-        ))}
+          ? requestDrink.slice(0, TWELVE).map((drink, index) => (
+            <div key={ drink.idDrink } data-testid={ `${index}-recipe-card` }>
+              <img
+                src={ drink.strDrinkThumb }
+                alt="imagem do drink"
+                data-testid={ `${index}-card-img` }
+              />
+              <p data-testid={ `${index}-card-name` }>{drink.strDrink}</p>
+            </div>
+          )) : <Recipes />}
       </div>
       <Footer />
     </div>
