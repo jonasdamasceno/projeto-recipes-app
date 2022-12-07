3d46a732929451b7162a4f23a9330b422083191b
Gabriela Gontijo <dev.gabigontijo@gmail.com>
2022-12-07 16:09:28 +0000

    :heavy_check_mark: feat: req 39

--- src/components/IngredientsProgress.js
+++ src/components/IngredientsProgress.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import '../style/IngredientsInProgress.css';
 
 export default function IngredientProgress(props) {
   const { recipeType } = props;
+
+  const handleCheckbox = ({ target }) => {
+    if (target.checked) {
+      target.parentElement.className = 'radio-ckecked';
+    }
+    if (!target.checked) {
+      target.parentElement.className = '';
+    }
+  };
 
   function renderIngredients(param1) {
     const asArray = Object.entries(recipeType);
@@ -28,11 +38,16 @@ export default function IngredientProgress(props) {
         <label
           key={ ingred }
           data-testid={ `${index}-ingredient-step` }
-          htmlFor="ingredients"
+          htmlFor={ ingred }
         >
           {ingred}
           {' '}
-          <input type="checkbox" name="ingredients" id="ingredients" />
+          <input
+            type="checkbox"
+            name={ ingred }
+            id={ ingred }
+            onChange={ handleCheckbox }
+          />
           {' '}
 
         </label>))}
