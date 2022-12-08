d0fa20a76f02dd78967e44d441691e7a17f5cdd9
Gabriela Gontijo <dev.gabigontijo@gmail.com>
2022-12-08 18:09:27 +0000

    :heavy_check_mark: feat: req 40

--- src/components/IngredientsProgress.js
+++ src/components/IngredientsProgress.js
@@ -12,30 +12,41 @@ export default function IngredientProgress(props) {
   // RIP = Recipe In Progress
   const [rip, setRip] = useState({ meals: { }, drinks: { } });
   const [checkedMap, setCheckedMap] = useState({});
+  const [started, setStarted] = useState(false);
 
   const isChecked = (param) => {
+    const newCheckedMap = {};
     if (pathname.includes('/meals')) {
-      param.meals[id].forEach((el) => {
-        checkedMap[el] = el;
+      param.meals[id]?.forEach((el) => {
+        newCheckedMap[el] = true;
       });
     }
     if (pathname.includes('/drinks')) {
-      param.drinks[id].forEach((el) => {
-        checkedMap[el] = el;
+      param.drinks[id]?.forEach((el) => {
+        newCheckedMap[el] = true;
       });
     }
-    setCheckedMap(checkedMap);
+    setCheckedMap(newCheckedMap);
   };
 
   useEffect(() => {
     let items;
-    if (Object.keys(rip.meals).length === 0
-      || (rip.drinks && Object.keys(rip.drinks).length === 0)) {
-      items = JSON.parse(localStorage.getItem('inProgressRecipes'));
-      isChecked(items);
+    console.log(JSON.parse(localStorage.getItem('inProgressRecipes')));
+    items = JSON.parse(localStorage.getItem('inProgressRecipes'));
+    items = items === null ? rip : items;
+    if (!started) {
+      if (!items.meals && items.drinks) {
+        items.meals = {};
+      } else if (!items.drinks && items.meals) {
+        items.drinks = {};
+      }
+      setRip(items);
+      setStarted(true);
     } else {
       items = rip;
     }
+
+    isChecked(items);
     saveRecipeInProgressLocalStorage(items);
   }, [rip]);
 
@@ -66,12 +77,12 @@ export default function IngredientProgress(props) {
       target.parentElement.className = '';
       if (pathname.includes('/meals')) {
         setRip({ ...rip,
-          meals: { [id]: rip.meals[id].filter((meal) => meal !== +target.name) },
+          meals: { [id]: rip.meals[id]?.filter((meal) => meal !== +target.name) },
           ...rip.drinks });
       }
       if (pathname.includes('/drinks')) {
         setRip({ ...rip,
-          drinks: { [id]: rip.drinks[id].filter((drink) => drink !== +target.name) },
+          drinks: { [id]: rip.drinks[id]?.filter((drink) => drink !== +target.name) },
           ...rip.meals });
       }
     }
@@ -82,23 +93,21 @@ export default function IngredientProgress(props) {
     if (pathname.includes('/meals')) {
       if (!rip.meals[id]) {
         setRip({ meals: { [id]: [+name] }, ...rip.drinks });
-        // saveRecipeInProgressLocalStorage(rip);
       } else {
         setRip({ ...rip,
           meals:
               { [id]: [...rip.meals[id], +name] } });
-        // saveRecipeInProgressLocalStorage(rip);
+        setCheckedMap(!checkedMap[+name]);
       }
     } if (pathname.includes('/drinks')) {
       if (!rip.drinks[id]) {
         setRip({ drinks: { [id]: [+name] }, ...rip.meals });
-        // saveRecipeInProgressLocalStorage(rip);
       } else {
         setRip({ ...rip,
           drinks:
               { [id]: [...rip.drinks[id], +name] } });
-        // saveRecipeInProgressLocalStorage(rip);
       }
+      setCheckedMap(!checkedMap[+name]);
     }
   };
 
@@ -109,6 +118,7 @@ export default function IngredientProgress(props) {
           key={ ingred }
           data-testid={ `${index}-ingredient-step` }
           htmlFor={ ingred }
+          className={ checkedMap[index] ? 'radio-ckecked' : '' }
         >
           {ingred}
           {' '}
