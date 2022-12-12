155d5b1435760620766c6acdad524c7c87381148
Gabriela Gontijo <dev.gabigontijo@gmail.com>
2022-12-12 23:59:04 +0000

    :test IngredientsInprogress

--- src/service/LocalStorage.js
+++ src/service/LocalStorage.js
@@ -1,15 +1,6 @@
-export const getLocalStorage = (param) => {
-  const tokenLocalStorage = localStorage.getItem(param);
-  return tokenLocalStorage;
-};
-
 export const saveUserLocalStorage = (param) => {
   localStorage.setItem('user', JSON.stringify(param));
 };
-
-// export const removeLocalStorage = (param) => {
-//   localStorage.removeItem(param);
-// };
 
 export const saveRecipeInProgressLocalStorage = (param) => {
   localStorage.setItem('inProgressRecipes', JSON.stringify(param));
